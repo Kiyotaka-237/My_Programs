48a5b4da08b0511a686068f42ae78989efef5cc1
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-24 16:05:26 +0100

    modified repitdig3

--- c_files/8/repitdeg3.c
+++ c_files/8/repitdeg3.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 #define N 10
 
 int main(void) {
-    bool digit_seen[N] = {false};
-    int occurence[N] = {0}, num, digit;
+    int occurence[N] = {0}, digit;
+    long num;
 
     printf("Enter number to check for repetition: ");
-    scanf("%d", &num);
+    scanf("%ld", &num);
 
-    while ( num > 0) {
-        digit = num % 10;
+    while (num > 0) {
+        while ( num > 0) {
+            digit = num % 10;
+            occurence[digit]++;
 
-        occurence[digit]++;
-
-        num /= 10;
-    }
+            num /= 10;
+        }
 
     printf("digit:\t    0 1 2 3 4 5 6 7 8 9\n");
     printf("occurences:");
     for (int i = 0; i < N; i++) {
         printf("%2d", occurence[i]);
+        occurence[i] = 0;
+    }
+    printf("\n");
+    printf("Enter number to check for repetition: ");
+    scanf("%ld", &num);
+    
     }
 
     printf("\n");
