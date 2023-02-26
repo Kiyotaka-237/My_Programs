a42c1cb1ba8b70d3ac5d345005cf342b68a2af6e
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-02-26 20:23:46 +0100

    :)

---
+++ c_files/13/date.c
@@ -0,0 +1,22 @@
+//prints a date entered
+#include <stdio.h>
+
+int main(void) {
+    const char *month[12] = {"January", "February", "March", "April", "May", "June",
+                        "July", "August", "September", "October", "November",
+                        "December"};
+    int mon, day, year;
+
+    printf("Enter a date (mm/dd/yyyy): ");
+    scanf("%d/%d/%d", &mon, &day, &year);
+
+    while ((mon > 12 || mon <= 0) || (day > 31 || day <= 0)) {
+        printf("invalide date!\n");
+        printf("Enter a date (mm/dd/yyyy): ");
+        scanf("%d/%d/%d", &mon, &day, &year);
+    }
+    printf("you entered the date %s %d, %d", month[mon - 1], day, year);
+
+    printf("\n");
+    return 0;
+}
