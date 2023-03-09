0dcb53c5a3b6072d92a01166bfc5642708e1996c
Kiyotaka-237 <draculemihawk232@gmail.com>
2023-03-09 18:23:31 +0100

    justify text

--- c_files/10/RPN_calc.c
+++ c_files/10/RPN_calc.c
@@ -1,11 +1,69 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define STACK_SIZE 100
+
+int top = 0;
+int contents[STACK_SIZE] = {0};
+
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void stack_push(char i);
+char stack_pop(void);
+void stack_overflow(void);
+void stack_underflow(void);
 
 int main(void) {
-    char i[5];
+    char ch;
 
-    printf("djdi: ");
-    for (int j = 0; j < 5; j++) {
-        scanf(" %c", &i[j]);
-        printf(" %c", i[j]);
+    printf("Enter RPN expression: ");
+
+    while ((ch = getchar()) != '\n') {
+        if (isdigit(ch)) {
+            stack_push(ch);
+        }
+        else
+        switch (ch) {
+            case '+': 
+        }
     }
+}
+
+void stack_overflow(void) {
+    printf("\nExpresion is too complex");
+    exit(EXIT_FAILURE);
+}
+
+void stack_underflow(void) {
+    printf("\nnot enough operands in expresion");
+    exit(EXIT_FAILURE);
+}
+
+void make_empty(void) {
+    top = 0;
+}
+
+void stack_push(char c) {
+    if (is_full())
+        stack_overflow();
+    else
+        contents[top++] = c;
+}
+
+char stack_pop(void) {
+    if (is_empty())
+        stack_underflow();
+    else
+        return contents[--top];
+}
+
+bool is_full(void) {
+    return top = STACK_SIZE;
+}
+
+bool is_empty(void) {
+    return top = 0;
 }
--- c_files/14/txt_justify/word.c
+++ c_files/14/txt_justify/word.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <word.h>
+#include "word.h"
 
 int read_char(void) {
 
